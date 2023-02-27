ac0b885b63ca44207009d441bf122220fc6cb66b
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-02-27 15:02:52 -0800

    Update UnitTest1.cs

--- PalindromeExercise/PalindromeExercise/UnitTest1.cs
+++ PalindromeExercise/PalindromeExercise/UnitTest1.cs
@@ -5,9 +5,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InLineData("racecar", true)]
+        [InLineData("hello", false)]
+        [InLineData("Racecar", false)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
+            var tester = new IsAPalindrome();
+            
+            //act
+            bool actual = tester.IsAPalindrome(word);
+            
+            //assert
+            Assert.Equal(expected, actual):
 
         }
     }
